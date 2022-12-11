58bf5bd1f9a660f8c4c9e80ef331d73ce4b0747a
Ana <ana.kasrashvili.1@iliauni.edu.ge>
2022-12-11 14:34:10 +0400

    lazy load children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { HomeComponent } from './home/home.component';
-import { NewsPage } from './news/pages/news.page';
-import { ToDoPage } from './todo/pages/todo.page';
 
+//? Without lazy loading, all three components (HomeComponent, UsersComponent, and ProductsComponent) would be loaded when the application is initialized, even if the user never navigates to the users or products routes.
+//* const routes: Routes = [
+//*   { path: 'home', component: HomeComponent },
+//*   { path: 'users', component: UsersComponent },
+//*   { path: 'products', component: ProductsComponent }
+//* ];
+
+//? lazy loading
 const routes: Routes = [
-  // when user goes to /my-app/ url, it will be redirected to /my-app/home
+  //? when user goes to /my-app/ url, it will be redirected to /my-app/home
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'news-page', component: NewsPage },
-  { path: 'todo-page', component: ToDoPage },
+  {
+    path: 'news-page',
+    loadChildren: () => import('./news/news.module').then((m) => m.NewsModule),
+  },
+  {
+    path: 'todo-page',
+    loadChildren: () => import('./todo/todo.module').then((m) => m.ToDoModule),
+  },
 ];
 
 @NgModule({
